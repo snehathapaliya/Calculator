cc06e4b3ab879488cbef74fe90e3f896e45e9258
snehathapaliya <shreyavshh@gmail.com>
2022-09-13 22:26:57 -0500

    fixed Classes Name

--- src/App.js
+++ src/App.js
@@ -45,20 +45,20 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <div classnmame="Calculator">
-        <div className="Display">
+    <div className="fi-App">
+      <div classnmame="fi-Calculator">
+        <div className="fi-Display">
           {result ? <span>({result})</span>: ''}&nbsp;
           { cal || "0" }
         </div>
-        <div className="operators">
+        <div className="fi-operators">
           <button onClick={()=> updatecal('/')}>/</button>
           <button onClick={()=> updatecal('*')}>*</button>
           <button onClick={()=> updatecal('+')}>+</button>
           <button onClick={()=> updatecal('-')}>-</button>
           <button onClick={deletelast}>Del</button>
         </div>
-        <div className="digits">
+        <div className="fi-digits">
           { createDigits() }
         <button onClick={()=> updatecal('0')}>0</button>
         <button onClick={()=> updatecal('.')}>.</button>
