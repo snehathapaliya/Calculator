5a3c88c9234f15fae815d41d5297844574d92e24
snehathapaliya <shreyavshh@gmail.com>
2022-09-13 22:20:13 -0500

    Added JS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
-
-
+import { useState } from 'react';
 
 function App() {
+  const [cal,setcal] = useState("");
+  const [result,setresult] = useState("");
+  
+  const ops =['/', '*', '+', '-', '.'];
+
+  const updatecal = value => {
+    if(
+       ops.includes(value) && cal === '' || ops.includes(value) && ops.includes(cal.slice(-1))
+      ) {
+          return;
+      }
+    setcal(cal + value);
+
+     if(!ops.includes(value)){
+      setresult(eval(cal + value).toString());
+     }
+  }
+
+  const createDigits = () => {
+
+    const digits = [];
+    
+    for (let i=1; i<10; i++){
+      digits.push(
+        <button onClick={()=> updatecal(i.toString())} key={i}>{i}</button>
+      )
+
+    }
+    return digits;
+  }
+
+  const calculate = () => {
+    setcal(eval(cal).toString());
+  }
+
+  const deletelast = () => {
+    if(cal === ''){
+      return;
+    }
+    const value = cal.slice(0,-1);
+    setcal(value);
+  }
+
   return (
     <div className="App">
       <div classnmame="Calculator">
